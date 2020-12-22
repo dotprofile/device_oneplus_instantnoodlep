d546976e5cb8847fca5dcacd83b3ae4383c00977
doc HD <doc.divxm@gmail.com>
2020-12-22 20:20:01 +0300

    instantnoodlep: regroup AICP specific overlays in overlay-aicp
    Change-Id: Ia49bef072260bc5a5675faebfa788d06fab976b6

--- device.mk
+++ device.mk
@@ -24,7 +24,8 @@
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-aicp
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
